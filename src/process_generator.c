ac6b56c2ac491b76b78d710740b4534401226e53
Moaaz Tarek <moaaztarek@hotmail.com>
2023-12-02 21:39:14 +0200

    choose algorithm

--- src/process_generator.c
+++ src/process_generator.c
@@ -1,19 +1,26 @@
 #include "headers.h"
 
-void clearResources(int);
-int **readInputFiles();
-enum SchedulingAlgorithms {HPF, SRTN, RR};
+typedef enum SchedulingAlgorithms {
+    HPF, SRTN, RR
+} SchedulingAlgorithm;
 
-int main(int argc, char * argv[])
-{
+void clearResources (int);
+int **readInputFiles ();
+SchedulingAlgorithm chooseSchedulingAlgorithm ();
+
+
+int main (int argc, char *argv[]) {
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
     int **processes;
     processes = readInputFiles();
+    
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
+    SchedulingAlgorithm algorithm = chooseSchedulingAlgorithm();
     
     // 3. Initiate and create the scheduler and clock processes.
+    
     // 4. Use this function after creating the clock process to initialize clock
 //    initClk();
     // To get time use this
@@ -26,28 +33,28 @@ int main(int argc, char * argv[])
 //    destroyClk(true);
 }
 
-void clearResources(int signum)
-{
+void clearResources (int signum) {
     //TODO Clears all resources in case of interruption
 }
 
-int **readInputFiles()
-{
+// @brief Reads the input files and returns a 2D array of processes
+// @return 2D array of processes
+int **readInputFiles () {
     FILE *file = fopen("processes.txt", "r");
     char *line = malloc(100 * sizeof(char));
-    int **processes = malloc(20 * sizeof(int*));
+    
+    int **processes = malloc(20 * sizeof(int *));
     for (int i = 0; i < 20; ++i) {
         processes[i] = malloc(4 * sizeof(int));
     }
+    
     int i = 0;
-    while (fgets(line, 100, file))
-    {
+    while (fgets(line, 100, file)) {
         if (line[0] == '#')
             continue;
         char *token = strtok(line, "\t");
         int j = 0;
-        while (token != NULL)
-        {
+        while (token != NULL) {
             processes[i][j] = atoi(token);
             token = strtok(NULL, "\t");
             j++;
@@ -55,5 +62,19 @@ int **readInputFiles()
     }
     
     return processes;
+}
+
+// @brief Asks the user for the chosen scheduling algorithm and its parameters, if there are any.
+SchedulingAlgorithm chooseSchedulingAlgorithm () {
+    int algorithm;
+    printf("Please choose the scheduling algorithm:\n");
+    printf("1- HPF\n");
+    printf("2- SRTN\n");
+    printf("3- RR\n");
+    do {
+        printf(">> ");
+        scanf("%d", &algorithm);
+    } while (algorithm < 1 || algorithm > 3);
     
+    return algorithm - 1;
 }
