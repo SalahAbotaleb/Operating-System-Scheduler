8cb73cc8513701d336bcc789526f110e380829b4
Salah <salah.mohamed@incorta.com>
2023-12-04 01:28:44 +0200

    initializing functions signature

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,6 +1,7 @@
 #include "headers.h"
-typedef u_int32_t ProcessID;
-typedef u_int32_t Time;
+typedef u_int16_t ProcessID;
+typedef u_int16_t Time;
+typedef u_int16_t Priority;
 
 typedef enum
 {
@@ -13,13 +14,43 @@ typedef struct
 {
     State state;
     ProcessID processID;
+    ProcessID mappedProcessID;
+    Time arrivalTime;
     Time startTime;
     Time remainingTime;
     Time finishTime;
+    Priority priority;
 
 } PCB;
 
 static void *createPCBEntry(Process *newProcess)
+{
+    PCB *newPCBEntry = (PCB *)malloc(sizeof(PCB));
+    newPCBEntry->arrivalTime = newProcess->arrivalTime;
+    newPCBEntry->remainingTime = newProcess->runTime;
+    newPCBEntry->priority = newProcess->priority;
+    newPCBEntry->processID = newProcess->id;
+    return newPCBEntry;
+}
+
+static void deletePCBEnrty(PCB *pcbEntry)
+{
+    free(pcbEntry);
+}
+
+static void addToRR(PCB *pcbEntry)
+{
+}
+
+static void addToHPF(PCB *pcbEntry)
+{
+}
+
+static void addToSRTN(PCB *pcbEntry)
+{
+}
+
+static receiveProcesses()
 {
 }
 
