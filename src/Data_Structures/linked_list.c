a572e86d9c8ea7b202900ca673989267ab2f2cd5
SalahAbotaleb <salah.m.abotaleb@gmail.com>
2023-12-04 01:02:14 +0200

    addind DS

---
+++ src/Data_Structures/linked_list.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+Node *CreateNode(void *obj)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = obj;
+    node->nxt = NULL;
+    node->prev = NULL;
+    return node;
+}
+
+void AddNodeToBack(LinkedList *listPtr, Node *nodePtr)
+{
+    if (listPtr->head == NULL)
+    {
+        listPtr->head = nodePtr;
+        listPtr->tail = nodePtr;
+    }
+    else
+    {
+        listPtr->tail->nxt = nodePtr;
+        nodePtr->prev = listPtr->tail;
+        listPtr->tail = nodePtr;
+    }
+}
+
+void AddNodeToFront(LinkedList *listPtr, Node *nodePtr)
+{
+    if (listPtr->head == NULL)
+    {
+        listPtr->head = nodePtr;
+        listPtr->tail = nodePtr;
+    }
+    else
+    {
+        listPtr->head->prev = nodePtr;
+        nodePtr->nxt = listPtr->head;
+        listPtr->head = nodePtr;
+    }
+}
+
+LinkedList *CreateLinkedList()
+{
+    LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
+}
+
+void *RemoveNodeFromFront(LinkedList *listPtr)
+{
+    Node *currNode = head;
+    if (listPtr->head == NULL)
+    {
+        return NULL;
+    }
+    else if (listPtr->head == listPtr->tail)
+    {
+        listPtr->head = NULL;
+        listPtr->tail = NULL;
+    }
+    else
+    {
+        Node *temp = listPtr->head;
+        listPtr->head = listPtr->head->nxt;
+        listPtr->head->prev = NULL;
+    }
+    return currNode;
+}
+
+void *RemoveNodeFromBack(LinkedList *listPtr)
+{
+    Node *currNode = tail;
+    if (listPtr->head == NULL)
+    {
+        return NULL;
+    }
+    else if (listPtr->head == listPtr->tail)
+    {
+        listPtr->head = NULL;
+        listPtr->tail = NULL;
+    }
+    else
+    {
+        Node *temp = listPtr->tail;
+        listPtr->tail = listPtr->tail->prev;
+        listPtr->tail->nxt = NULL;
+    }
+    return currNode;
+}
+
+void *RemoveNode(LinkedList *listPtr, Node *nodePtr)
+{
+    if (nodePtr == listPtr->head)
+    {
+        return RemoveNodeFromFront(listPtr);
+    }
+    else if (nodePtr == listPtr->tail)
+    {
+        return RemoveNodeFromBack(listPtr);
+    }
+    else
+    {
+        nodePtr->prev->nxt = nodePtr->nxt;
+        nodePtr->nxt->prev = nodePtr->prev;
+    }
+    return nodePtr;
+}
