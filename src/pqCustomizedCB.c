cad553671863a0835c15bcc24537728a688bc474
Salah <salah.mohamed@incorta.com>
2023-12-08 20:36:03 +0200

    adding priority queue callbacks

---
+++ src/pqCustomizedCB.c
@@ -0,0 +1,45 @@
+#include "Data_Structures/pqueue.h"
+#include "headers.h"
+/**Priority Queue Callback functions**/
+static int
+cmpPriority(pqueue_pri_t next, pqueue_pri_t curr)
+{
+    return (next > curr);
+}
+
+static pqueue_pri_t
+getPriority(void *a)
+{
+    return ((PCB *)a)->priority;
+}
+
+static pqueue_pri_t
+getRemainingTimeAsPriority(void *a)
+{
+    return ((PCB *)a)->remainingTime;
+}
+
+static void
+setPriority(void *a, pqueue_pri_t pri)
+{
+    ((PCB *)a)->priority = pri;
+}
+
+static void
+setRemainingTimeAsPriority(void *a, pqueue_pri_t pri)
+{
+    ((PCB *)a)->remainingTime = pri;
+}
+
+static size_t
+get_pos(void *a)
+{
+    return ((PCB *)a)->pq_position;
+}
+
+static void
+set_pos(void *a, size_t pos)
+{
+    ((PCB *)a)->pq_position = pos;
+}
+/**End of Priority Queue Callback functions**/
