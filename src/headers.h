8a69910272743734db66d09e06317a1a40038559
hussein elhawary <husseinelhawary@gmail.com>
2023-12-15 00:27:42 +0200

    RRprocess deadlock

--- src/headers.h
+++ src/headers.h
@@ -23,7 +23,7 @@ typedef short bool;
 #define MSG_QUEUE_SHCEDULAR_PROCESS_KEY 107
 #define MSG_QUEUE_GENERATOR_SCHEDULAR_KEY 503
 #define KEY_FILE "keyfile"
-#define MAX_NUM_OF_PROCESS 2
+#define MAX_NUM_OF_PROCESS 5
 #define PROCESS_EXECUTABLE_NAME "process.out"
 #define MAX_NUM_OF_RESOURCES 5
 typedef struct buffer_item
